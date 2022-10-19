bba7dc7b653bdf336af2de364aff81805b208fdd
unknown <mnonelelinquma91@gmail.com>
2022-10-19 10:24:13 +0200

    add js,name,amount,addExpenseBtn,expenses

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Aplication started');
+
+let inputName = document.getElementById('name');
+let inputAmount = document.getElementById('amount');
+let btnAdd = document.getElementById('addExpenseBtn');
+let btnExpense = document.getElementById('expenses');
+
+let incomelistDom = document.getElementById('income-list');
+let expenselistDom = document.getElementById('expense-list');
